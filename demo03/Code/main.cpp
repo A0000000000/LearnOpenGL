f7d44343ea2e8c4cb5251060b9c098c4b3b7001b
A0000000000 <mengxin_zzl@outlook.com>
2020-04-29 18:20:12 +0800

    colorful

---
+++ demo03/Code/main.cpp
@@ -0,0 +1,64 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Shader.h"
+int main(int argc, char* argv[])
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height)
+		{
+			glViewport(0, 0, width, height);
+		});
+	gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
+
+	float vertices[] = {
+		 0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+
+	unsigned vao, vbo;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+	glEnableVertexAttribArray(0);
+
+	Shader s("vertex.shader", "fragment.shader");
+	
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		[](GLFWwindow* window)
+		{
+			if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			{
+				glfwSetWindowShouldClose(window, true);
+			}
+		}(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		s.use();
+		float timeValue = glfwGetTime();
+		float value = sin(timeValue) / 2.0f + 0.5f;
+		s.setFloat("greenValue", value);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	
+	return 0;
+}
